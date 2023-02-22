4bd312394cba7477b9c45096c183c29c226b9921
diaba <hwar@cincinnatistate.edu>
2023-02-22 18:14:22 -0500

    demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentListComponent } from './student-list/student-list.component';
+import { TeacherListComponent } from './teacher-list/teacher-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'student',
+    component: StudentListComponent,
+  },
+  {
+    path: 'teacher',
+    component: TeacherListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/teacher-list/teacher-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-list',
+  template: `
+    <p>
+      teacher-list works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TeacherListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
